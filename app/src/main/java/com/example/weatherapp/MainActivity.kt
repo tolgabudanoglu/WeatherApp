dcc74db3922a612dc5dc95026d17cefd8b138ab8
tolga budanoğlu <budanoglu.tolga@gmail.com>
2023-03-28 17:26:59 +0300

    Location

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -6,23 +6,85 @@ import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
 import com.android.volley.Response
-import com.android.volley.toolbox.JsonObjectRequest
-import org.json.JSONObject
-import com.android.volley.toolbox.Volley
-import com.android.volley.Request;
 import com.android.volley.VolleyError
+import com.android.volley.toolbox.JsonObjectRequest
 import com.example.weatherapp.databinding.ActivityMainBinding
+import im.delight.android.location.SimpleLocation
+import org.json.JSONObject
 import java.text.SimpleDateFormat
 import java.util.*
 
 
 class MainActivity : AppCompatActivity() , AdapterView.OnItemSelectedListener{
 
+
+    var location:SimpleLocation?=null
+    var latitude:String? = null
+    var longitude:String? = null
+
+
     private lateinit var binding: ActivityMainBinding
+    private fun currentCity(lat: String?, longitude: String?) {
+
+        val url = "https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+longitude+"&appid=dbebd51d2e67e2d589db303fb923efad&lang=tr&units=metric"
+        val weatherObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,object : Response.Listener<JSONObject> {
+
+
+            override fun onResponse(response: JSONObject?) {
+
+                var main = response?.getJSONObject("main")
+                var tempereture = main?.getInt("temp")
+                binding.tvTemp.text = tempereture.toString()
+
+                var cityName = response?.getString("name")
+
+                var weather=response?.getJSONArray("weather")
+                var descrition = weather?.getJSONObject(0)?.getString("description")
+
+                binding.tvDescription.text = descrition
+                var icon = weather?.getJSONObject(0)?.getString("icon")
+
+                if (icon?.last() == 'd'){
+                    binding.rootLayout.background = getDrawable(R.drawable.bg)
+                }else{
+                    binding.rootLayout.background = getDrawable(R.drawable.gece)
+                    binding.tvHistory.setTextColor(resources.getColor(R.color.colorAccent))
+                    binding.tvDescription.setTextColor(resources.getColor(R.color.colorAccent))
+                    binding.tvCelcius.setTextColor(resources.getColor(R.color.colorAccent))
+                    binding.tvTemp.setTextColor(resources.getColor(R.color.colorAccent))
+                }
+
+
+
+                var imageFile = resources.getIdentifier("icon_"+icon?.lastDigitDelete(),"drawable",packageName)
+
+                binding.imgWeather.setImageResource(imageFile)
+
+                binding.tvHistory.text = history()
+
+
+
+
+
+            }
+
+
+        }, object : Response.ErrorListener {
+
+            override fun onErrorResponse(error: VolleyError?) {
+
+            }
+
+        })
+
+
+        MySingleton.getInstance(this).addToRequestQueue(weatherObjectRequest)
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -35,11 +97,41 @@ class MainActivity : AppCompatActivity() , AdapterView.OnItemSelectedListener{
 
         binding.spnCity.onItemSelectedListener = this
 
+        location = SimpleLocation(this)
+
+        if (!location!!.hasLocationEnabled()) {
+            // ask the user to enable location access
+            SimpleLocation.openSettings(this);
+        }
+        location?.setListener(object :SimpleLocation.Listener{
+            override fun onPositionChanged() {
+
+                 latitude = String.format("%.2f",location?.latitude)
+                 longitude = String.format("%.2f",location?.longitude)
+
+                Log.e("tolgaaaaaaaa ",""+latitude + " " + longitude)
+            }
+
+        })
+
+
 
         data("muğla")
+        currentCity(latitude,longitude)
 
 
 
+    }
+
+    override fun onResume() {
+        super.onResume()
+        location?.beginUpdates()
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        location?.endUpdates()
     }
 
     fun data(sehir:String){
